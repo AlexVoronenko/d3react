7632221848d6de758d276d135082638825a9019b
Alexander Voronenko <796546@gmail.com>
2023-09-05 11:54:39 +0300

    add aspectRatio for Axis canvas

--- src/Modules/Axis.js
+++ src/Modules/Axis.js
@@ -3,17 +3,20 @@ import "./css/axisCss.css";
 
 function Axis(props) {
   let data = props.data;
-  const aspectRatio = 1;
+  const aspectRatio = 1920 / 1080;
   const canvasHeight = 600;
   const canvasWidth = canvasHeight * aspectRatio;
   const margin = { top: 10, right: 20, bottom: 30, left: 20 };
   const height = canvasHeight - margin.top - margin.bottom;
   const width = canvasWidth - margin.left - margin.right;
 
+  let scaleSvg = height / 50 / 2;
+  let domainSize = height / (2 * scaleSvg);
+
   const x = d3.scaleLinear().range([0, width]);
-  x.domain([-10, 10]);
+  x.domain([-domainSize, domainSize]);
   const y = d3.scaleLinear().range([0, height]);
-  x.domain([-10, 10]);
+  y.domain([-domainSize / aspectRatio, domainSize / aspectRatio]);
 
   // let scaleSvg = height / 2;
   let translateX = width / 2;
@@ -28,8 +31,13 @@ function Axis(props) {
     .append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
-  const xAxis = d3.axisBottom(x).ticks(16).tickSize(-height).tickPadding(10);
-  const yAxis = d3.axisLeft(y).ticks(16).tickSize(-width);
+  const ticksCount = 16;
+  const xAxis = d3.axisBottom(x).ticks(ticksCount).tickSize(-height).tickPadding(10);
+  const yAxis = d3
+    .axisLeft(y)
+    .ticks(ticksCount / aspectRatio)
+    .tickSize(-width)
+    .tickPadding(5);
 
   svg
     .append("g")
@@ -81,11 +89,11 @@ function Axis(props) {
       .attr("class", "circle")
       .attr("cx", (d) => d.x)
       .attr("cy", (d) => d.y)
-      .attr("r", 4)
+      .attr("r", 4 / scaleSvg)
       .attr("stroke", "black")
       .attr("fill", "none")
       .attr("stroke-width", 1)
-      // .attr("transform", ` translate(${translateX},${translateY}) scale(${scaleSvg},-${scaleSvg})`)
+      .attr("transform", ` translate(${translateX},${translateY}) scale(${scaleSvg},-${scaleSvg})`)
       .style("stroke", "blue");
 
     console.log(data);
